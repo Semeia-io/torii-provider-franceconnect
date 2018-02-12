60fb373fa6feafe357d5cf72d73c9af62815a991
Julien Palmas <julien.palmas@gmail.com>
2018-02-12 08:19:44 +0100

    initial

---
+++ addon/torii-providers/franceconnect-oauth2.js
@@ -0,0 +1,15 @@
+import Oauth2 from 'torii/providers/oauth2-code';
+import { configurable } from 'torii/configuration';
+
+export default Oauth2.extend({
+  name:    'franceconnect',
+  baseUrl: 'https://fcp.integ01.dev-franceconnect.fr/api/v1/authorize',
+  nonce: configurable('nonce'),
+
+  init() {
+    this.requiredUrlParams = this.requiredUrlParams.concat('nonce');
+    this.optionalUrlParams = ['scope'];
+    this.responseParams = ['code', 'state'];
+  }
+
+});
