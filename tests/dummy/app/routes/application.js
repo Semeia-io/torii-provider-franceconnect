33aa9cbf4729bcb1932960e2bce9b263800fa949
Julien Palmas <julien.palmas@gmail.com>
2018-02-21 23:38:33 +0100

    cleanup: remove debug traces

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -3,9 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   actions: {
     login() {
-      this.get('torii').open('franceconnect').then((sdf) => {
-        console.log(sdf);
-      })
+      this.get('torii').open('franceconnect');
     }
   }
 });
