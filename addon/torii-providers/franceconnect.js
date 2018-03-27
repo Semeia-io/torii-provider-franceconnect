65157c877361ae793eede9ccfd40f8e20ff90dfa
Julien Palmas <julien.palmas@gmail.com>
2018-03-27 11:37:21 +0200

    fix for ember 3.0

--- addon/torii-providers/franceconnect.js
+++ addon/torii-providers/franceconnect.js
@@ -1,3 +1,5 @@
+/* eslint ember/avoid-leaking-state-in-ember-objects: "off" */
+
 import Oauth2 from 'torii/providers/oauth2-code';
 import { configurable } from 'torii/configuration';
 
@@ -5,11 +7,11 @@ export default Oauth2.extend({
   name:    'franceconnect',
   baseUrl: 'https://fcp.integ01.dev-franceconnect.fr/api/v1/authorize',
   nonce: configurable('nonce'),
+  responseParams: ['code', 'state'],
 
   init() {
     this.requiredUrlParams = this.requiredUrlParams.concat('nonce');
     this.optionalUrlParams = ['scope'];
-    this.responseParams = ['code', 'state'];
   },
 
   fetch(data) {
